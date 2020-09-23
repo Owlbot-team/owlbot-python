66fc12bc26701e3a95afd16395c2a72169df7d56
Eliot <eliotberesfordpersonal@gmail.com>
2020-09-23 16:38:16 +0100

    Conform file to PEP 8
    PEP 8 is a Python style guide that helps to unify how code looks
    across developers, thus making reading/writing universally more easy.
    
    I've just made some stylistic changes. Everything should still run the
    same.
    
    Lots of info here: https://www.python.org/dev/peps/pep-0008/
    
    P.S. Really, the trick is to get a linter, which will check your code
    against PEP 8 for you, rather than memorising all the guidelines off
    by heart :)

--- testAPI2/TestAPI.py
+++ testAPI2/TestAPI.py
@@ -10,32 +10,45 @@ value = 0
 xValue = 0
 zValue = 0
 
+
 def MoveCameraOwl():
- print("Camera: move to OWL")
- value = 4
- xValue = 147
- zValue = 66
- response = requests.get(url, params={'value': str(value), 'xyval': str(xValue), 'zval': str(zValue), 'token': token})
-  # Get the response data as a python object. Verify that it's a dictionary.
- data = response.json()
- print(type(data))
- # print the return JSON string
- print(data)
- time.sleep(5);
+    print("Camera: move to OWL")
+    value = 4
+    xValue = 147
+    zValue = 66
+    response = requests.get(url, params={
+        'value': str(value),
+        'xyval': str(xValue),
+        'zval': str(zValue),
+        'token': token
+    })
+    # Get the response data as a python object. Verify that it's a dictionary.
+    data = response.json()
+    print(type(data))
+    # print the return JSON string
+    print(data)
+    time.sleep(5)
+
 
 def MoveCameraLight():
- print("Camera: move to Howard")
- value = 4
- xValue = 16
- zValue = 57
- 
- response = requests.get(url, params={'value': str(value), 'xyval': str(xValue), 'zval': str(zValue), 'token': token})
-  # Get the response data as a python object. Verify that it's a dictionary.
- data = response.json()
- print(type(data))
- # print the return JSON string
- print(data)
- time.sleep(5);
+    print("Camera: move to Howard")
+    value = 4
+    xValue = 16
+    zValue = 57
+
+    response = requests.get(url, params={
+        'value': str(value),
+        'xyval': str(xValue),
+        'zval': str(zValue),
+        'token': token
+    })
+
+    # Get the response data as a python object. Verify that it's a dictionary.
+    data = response.json()
+    print(type(data))
+    # print the return JSON string
+    print(data)
+    time.sleep(5)
 
 
 root = tk.Tk()
@@ -49,20 +62,16 @@ button = tk.Button(frame,
                    command=quit)
 button.pack(side=tk.LEFT)
 
-Camera_OWL = tk.Button(frame,
-                  text="OWL",
-                   command=MoveCameraOwl)
+Camera_OWL = tk.Button(frame, text="OWL", command=MoveCameraOwl)
 Camera_OWL.pack(side=tk.RIGHT)
 
-Camera_LIGHT = tk.Button(frame,
-                   text="Howard",
-                   command=MoveCameraLight)
+Camera_LIGHT = tk.Button(frame, text="Howard", command=MoveCameraLight)
 Camera_LIGHT.pack(side=tk.RIGHT)
 
 frame.pack()
 root.mainloop()
 
-#if response.status_code == 200:
+# if response.status_code == 200:
 #    print('Success!')
-#elif response.status_code == 404:
+# elif response.status_code == 404:
 #    print('Not Found.')
