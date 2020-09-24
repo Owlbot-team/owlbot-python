f011e9f49f775d56ad7b4b1a409fd6495fa7cbba
Eliot <eliotberesfordpersonal@gmail.com>
2020-09-24 01:15:57 +0100

    Move camera move functions into a class
    Having the camera functions in a class will help things to be more neat
    and more scalable, and help the code to work with itself.
    
    I'll clean things up over the next few commits.

--- testAPI2/TestAPI.py
+++ testAPI2/TestAPI.py
@@ -10,84 +10,88 @@ token = 'Eliot'
 is_cam_movable = True
 
 
-def verify_dict(data):
-    """Returns True if data is of type dict, otherwise returns False"""
-    if type(data) is dict:
-        return True
-    else:
-        print("Error: data was not a dict, was instead {}".format(type(data)))
-        return False
+class Camera:
+    def __init__(self):
+        pass
 
+    def verify_dict(self, data):
+        """Returns True if data is of type dict, otherwise returns False"""
+        if type(data) is dict:
+            return True
+        else:
+            print("Error: data was not a dict, was instead "
+                  "{}".format(type(data)))
+            return False
 
-def five_sec_timer():
-    time.sleep(5)
-    global is_cam_movable
-    is_cam_movable = True
+    def five_sec_timer(self):
+        """Times for 5 seconds"""
+        time.sleep(5)
+        global is_cam_movable
+        is_cam_movable = True
 
+    def MoveCameraOwl(self):
+        print("Camera: move to OWL")
+        global is_cam_movable
+        value = 4
+        xValue = 147
+        zValue = 66
 
-def MoveCameraOwl():
-    print("Camera: move to OWL")
-    global is_cam_movable
-    value = 4
-    xValue = 147
-    zValue = 66
+        if not is_cam_movable:
+            print("Error: You can\'t do that yet! Please wait.")
+            return
 
-    if not is_cam_movable:
-        print("Error: You can\'t do that yet! Please wait.")
-        return
+        response = requests.get(url, params={
+            'value': str(value),
+            'xyval': str(xValue),
+            'zval': str(zValue),
+            'token': token
+        })
 
-    response = requests.get(url, params={
-        'value': str(value),
-        'xyval': str(xValue),
-        'zval': str(zValue),
-        'token': token
-    })
+        # Get the response data
+        data = response.json()
 
-    # Get the response data
-    data = response.json()
+        # Verify that it's a dictionary
+        if not self.verify_dict(data):
+            return
 
-    # Verify that it's a dictionary
-    if not verify_dict(data):
-        return
+        # Print the data
+        print(data)
+        is_cam_movable = False
+        Thread(target=self.five_sec_timer).start()
 
-    # Print the data
-    print(data)
-    is_cam_movable = False
-    Thread(target=five_sec_timer).start()
+    def MoveCameraLight(self):
+        print("Camera: move to Howard")
+        global is_cam_movable
+        value = 4
+        xValue = 16
+        zValue = 57
 
+        if not is_cam_movable:
+            print("Error: You can\'t do that yet! Please wait.")
+            return
 
-def MoveCameraLight():
-    print("Camera: move to Howard")
-    global is_cam_movable
-    value = 4
-    xValue = 16
-    zValue = 57
+        response = requests.get(url, params={
+            'value': str(value),
+            'xyval': str(xValue),
+            'zval': str(zValue),
+            'token': token
+        })
 
-    if not is_cam_movable:
-        print("Error: You can\'t do that yet! Please wait.")
-        return
+        # Get the response data
+        data = response.json()
 
-    response = requests.get(url, params={
-        'value': str(value),
-        'xyval': str(xValue),
-        'zval': str(zValue),
-        'token': token
-    })
+        # Verify that it's a dictionary
+        if not self.verify_dict(data):
+            return
 
-    # Get the response data
-    data = response.json()
-
-    # Verify that it's a dictionary
-    if not verify_dict(data):
-        return
-
-    # Print the data
-    print(data)
-    is_cam_movable = False
-    Thread(target=five_sec_timer).start()
+        # Print the data
+        print(data)
+        is_cam_movable = False
+        Thread(target=self.five_sec_timer).start()
 
 
 root = tk.Tk()
+camera = Camera()
 
 frame = tk.Frame(root, width=320, height=240)
 frame.pack_propagate(False)
@@ -95,10 +99,10 @@ frame.pack_propagate(False)
 button = tk.Button(frame, text="Quit", fg="dark green", command=quit)
 button.pack(side=tk.LEFT)
 
-Camera_OWL = tk.Button(frame, text="OWL", command=MoveCameraOwl)
+Camera_OWL = tk.Button(frame, text="OWL", command=camera.MoveCameraOwl)
 Camera_OWL.pack(side=tk.RIGHT)
 
-Camera_LIGHT = tk.Button(frame, text="Howard", command=MoveCameraLight)
+Camera_LIGHT = tk.Button(frame, text="Howard", command=camera.MoveCameraLight)
 Camera_LIGHT.pack(side=tk.RIGHT)
 
 frame.pack()
